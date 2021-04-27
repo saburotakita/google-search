44e7933aecc5fa2f5a1adeac421c617549a63b01
Saburo Takita <saburo_takita@outlook.jp>
2021-04-27 19:18:58 +0900

    create version 1

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import datetime
+import pathlib
 
 import eel
 from browser.chrome import Chrome
@@ -17,13 +18,33 @@ def main():
 
 @eel.expose
 def search(text, count):
+    if not text.split():
+        eel.change_message('error', '検索ワードを入力してください')
+        return None
+
+    if not count.isdecimal() or int(count) <= 0:
+        eel.change_message('error', '最大取得件数は１以上の数値で入力してください')
+        return None
+
+    eel.change_message('running', '実行中です・・・')
+    eel.change_search_button('disable')
+
     count = int(count)
-    with GoogleSearch(Chrome(False)) as conn:
+    with GoogleSearch(Chrome(True)) as conn:
         now = datetime.datetime.now()
         results = conn.search(text, count)
-        df = pd.DataFrame(results)
-        df.index = df.index + 1
-        df.to_csv(f'result_{now.strftime("%Y%m%d_%H%M%S")}.csv', encoding='utf-8_sig')
+
+    df = pd.DataFrame(results)
+    df.index = df.index + 1
+    
+    dir_path = pathlib.Path('results')
+    dir_path.mkdir(exist_ok=True)
+
+    file_path = dir_path / f'result_{now.strftime("%Y%m%d_%H%M%S")}.csv'
+    df.to_csv(file_path, encoding='utf-8_sig')
+
+    eel.change_message('success', '完了しました')
+    eel.change_search_button('enable')
 
 
 if __name__ == '__main__':
--- web/js/myscript.js
+++ web/js/myscript.js
@@ -1,5 +1,36 @@
 $(function(){
-    $btn = $("#btn-search").click(() => {
+    eel.expose(change_message)
+    function change_message(type, text) {
+        const $message = $("#message");
+
+        $message.removeClass("alert-primary");
+        $message.removeClass("alert-success");
+        $message.removeClass("alert-danger");
+        $message.removeClass("alert-secondary");
+
+        if (type === "running") {
+            $message.addClass("alert-primary");
+        } else if (type === "success") {
+            $message.addClass("alert-success");
+        } else if (type === "error") {
+            $message.addClass("alert-danger");
+        } else {
+            $message.addClass("alert-secondary");
+        }
+
+        $message.text(text)
+    }
+
+    eel.expose(change_search_button)
+    function change_search_button(is_disable) {
+        if (is_disable === "disable") {
+            $("#btn-search").prop("disabled", true);
+        } else {
+            $("#btn-search").prop("disabled", false);
+        }
+    }
+
+    $("#btn-search").click(() => {
         const searchText = $("#input-search-text").val();
         const count = $("#input-count").val();
         eel.search(searchText, count)
